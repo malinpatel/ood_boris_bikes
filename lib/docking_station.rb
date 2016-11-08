36b9e918ffad939fe54c4716e660f2ffddf4ddf1
Leke Abolade <leke_abolade92@hotmail.co.uk>
2016-11-08 20:33:25 +0000

    writing test for docking bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,18 @@
+
+
 class DockingStation
+
+  attr_reader :bike
 
   def release_bike
     Bike.new
   end
+
+  def return_bike(bike)
+     bike
+  end
+
+
 
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,14 +4,19 @@ describe DockingStation do
 
   describe "Check if a bike is released" do
   subject {docking_station = DockingStation.new}
-    it {is_expected.to respond_to (:release_bike) }
+    it {is_expected.to respond_to(:release_bike) }
+    it { bike = Bike.new; expect(subject.return_bike(bike)).to eq bike}
+
   end
 
   describe "Check if bike is working" do
     subject {bike_one = Bike.new}
     it {is_expected.to respond_to (:working?)}
   end
-  
 
+  # describe "Return a bike to the docking station" do
+  #   subject {docking_station = DockingStation.new}
+  #   it {is_expected.to respond_to (:return_bike) }
+  # end
 
 end
