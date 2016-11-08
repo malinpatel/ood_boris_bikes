439fcfccb0cd24409bf97916aa2d153dbe730cc4
malin patel <malin.patel@icloud.com>
2016-11-08 15:43:23 +0000

    RSpec one liner syntax test

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,6 +1,7 @@
 require 'docking_station'
 
-describe DockingStation do
-
-
+RSpec.describe DockingStation do
+  describe "when first created" do
+    it { should respond_to (release_bike) }
+  end
 end
