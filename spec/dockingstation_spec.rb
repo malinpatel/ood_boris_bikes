55871292407dd7ab1ddd05c6f6766aedc8b77cae
malin patel <malin.patel@icloud.com>
2016-11-08 17:43:30 +0000

    RSpec tests to check if DockingStation gets bike and if bike is working

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,6 +2,16 @@ require 'docking_station'
 
 describe DockingStation do
 
-    it { should respond_to (:release_bike) }
+  describe "Check if a bike is released" do
+  subject {docking_station = DockingStation.new}
+    it {is_expected.to respond_to (:release_bike) }
+  end
+
+  describe "Check if bike is working" do
+    subject {bike_one = Bike.new}
+    it {is_expected.to respond_to (:working?)}
+  end
+  
+
 
 end
