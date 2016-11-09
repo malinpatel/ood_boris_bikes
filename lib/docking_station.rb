3b1de3b26d4df090d9f51e8f9513b84f0e74e4ab
malin patel <malin.patel@icloud.com>
2016-11-09 15:00:09 +0000

    Exercise 14 complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -10,16 +10,23 @@ class DockingStation
   end
 
   def release_bike
-    fail "Rack is empty" if @bikes.empty?
+    fail "Rack is empty" if empty?
     return @bikes.pop
   end
 
   def return_bike(bike)
-    fail "Rack is full" unless @bikes.size < @max_bikes
+    fail "Rack is full" if full?
     @bikes.push bike
   end
 
+  private
+  def full?
+     @bikes.size >= @max_bikes
+  end
 
+  def empty?
+    @bikes.empty?
+  end
 
 
 end
