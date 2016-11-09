785da508584b80a5e1934addf9fdeb269fcfff8b
ejatkin <ejatkin@gmail.com>
2016-11-09 17:00:23 +0000

    Up to section 10 - working with @gweaton

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,9 @@
+require 'docking_station'
+
+describe DockingStation do
+  it { is_expected.to respond_to "release_bike" }
+  it "creates an instance of class Bike, and checks it is working" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+end
