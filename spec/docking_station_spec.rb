fc71afef2d68925fd9176eb49324ae905409a4cb
malin patel <malin.patel@icloud.com>
2016-11-11 10:32:06 +0000

    Exercise 19 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,18 +8,18 @@ describe DockingStation do
 
   describe '#dock_bike' do
     it "creates an instance of class Bike, and checks it is working" do
-      bike = subject.dock_bike(Bike.new)
+      bike = subject.dock_bike double :bike
       expect(bike.pop).to be_working
     end
 
     it 'checks that it docks a bike' do
-      bike = Bike.new
-      expect(subject.dock_bike(bike)).to include bike
+      bike = double :bike
+      expect(subject.dock_bike(bike)).to eq([bike])
     end
 
     it 'stops bikes being docked if the dock is full' do
-      subject.capacity.times {subject.dock_bike Bike.new}
-      expect{subject.dock_bike(Bike.new)}.to raise_error("The dock is full.")
+      subject.capacity.times {subject.dock_bike double :bike}
+      expect{subject.dock_bike(double :bike)}.to raise_error("The dock is full.")
     end
   end
 
