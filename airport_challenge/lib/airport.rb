25b859c3c48f17e66000e380da706845efe75e17
malin patel <malin.patel@icloud.com>
2016-11-13 22:35:30 +0000

    User stories 3, 4, 5

--- airport_challenge/lib/airport.rb
+++ airport_challenge/lib/airport.rb
@@ -1,29 +1,34 @@
 require 'plane.rb'
 
 class Airport
-#
-# attr_accessor :airport, :capacity
-#
-# DEFAULT_CAPACITY = 50
 
-  # def initialize(capacity = DEFAULT_CAPACITY)
-  #   @airport = []
-  #   @capacity = capacity
-  # end
+attr_accessor :planes, :capacity
+
+DEFAULT_CAPACITY = 50
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @planes = []
+    @capacity = capacity
+  end
 
   def land(plane)
-    # @airport << plane unless full?
+    fail "No space to land, need a plane to take off first." if full?
+    fail "Cannot land plane: weather is stormy." if stormy?
+    @planes << plane unless full?
   end
 
   def take_off(plane)
-    # @flying = true
-    # @airport.pop
+    fail "Cannot take off: weather is stormy." if stormy?
   end
-#
-# private
-#
-#   def full?
-#     fail "The airport is full, cannot land here!" if @airport.length >= @capacity
-#   end
+
+private
+
+  def full?
+     @planes.length >= @capacity
+  end
+
+  def stormy?
+    rand(1..6) > 4
+  end
 
 end
--- airport_challenge/spec/airport_spec.rb
+++ airport_challenge/spec/airport_spec.rb
@@ -9,17 +9,19 @@ describe Airport do
   describe 'Landing planes' do
 
     it 'instructs planes to take off' do
+      allow(airport).to receive(:stormy?).and_return false
       expect(airport).to respond_to(:land).with(1).argument
     end
 
-    # it 'should confirm a plane has landed' do
-    #   airport.land(plane)
-    #   expect(airport.airport).to eq([plane])
-    # end
-
     it 'should prevent planes from landing when the airport is full' do
+      allow(airport).to receive(:stormy?).and_return false
       airport.capacity.times {airport.land(plane)}
-      expect{airport.land(plane)}.to raise_error("The airport is full, cannot land here!")
+      expect{airport.land(plane)}.to raise_error("No space to land, need a plane to take off first.")
+    end
+
+    it 'prevents planes landing when stormy' do
+      allow(airport).to receive(:stormy?).and_return true
+      expect{airport.land(plane)}.to raise_error("Cannot land plane: weather is stormy.")
     end
 
   end
@@ -27,17 +29,17 @@ describe Airport do
   describe 'Planes taking off' do
 
     it 'should tell planes to take off' do
+      allow(airport).to receive(:stormy?).and_return false
       expect(airport).to respond_to(:take_off).with(1).argument
     end
 
-    # it 'should confirm a plane has taken off' do
-    #   airport.take_off(plane)
-    #   expect(subject.take_off(plane)).to eq(plane)
-    # end
-  end
+    it 'raises an error when trying to land the plane when stormy' do
+      allow(airport).to receive(:stormy?).and_return true
+      expect{airport.take_off(plane)}.to raise_error("Cannot take off: weather is stormy.")
+    end
 
   # describe 'The capacity of the airports can be changed' do
   #   expect(subject.new(60).capacity).to eq(60)
   # end
-
-end
+  end
+end 
